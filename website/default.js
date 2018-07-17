f634f7601419e85c73106c70742287cf068d05f0
shenikiar <shenikiar@gmail.com>
2018-07-17 18:21:41 -0400

    added extra files

---
+++ website/default.js
@@ -0,0 +1,5 @@
+
+/*remove this to allow message to pop up.......
+alert("Hey, Welcome to my page!"); break
+document.write("<h1>Welcome to JavaScript Basics</h1>");
+alert("Thanks for visiting.");
---
+++ website/extra.js
@@ -0,0 +1,14 @@
+var message = "Hello!";
+alert(message);
+
+prompt("What is your name?")
+
+var visitor = prompt("What is your name?");
+var message = 'Hello ' + visitor + ". Welcome to my page." + " I am so glad that you came to visit, " + visitor + ". Please come again.";
+document.write(message);
+
+
+/*can be placed in html
+<script>
+alert("Hi, Welcome to my page!");
+</script>
