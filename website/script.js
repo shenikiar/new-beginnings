7b1bc0352e73d280219dc416475b5f8170ad7b90
shenikiar <39305056+shenikiar@users.noreply.github.com>
2018-07-27 09:16:10 -0400

    Update script.js

--- website/script.js
+++ website/script.js
@@ -1,13 +1,12 @@
 $("div:hidden:first").fadeIn(1000).delay(4000).slideUp(1000);
 
 
-<script>
+
 function myFunction() {
-    document.getElementById("reg1").innerHTML=
+    document.getElementById("p").innerHTML=
     "Thank you for signing up!";
-    document.getElementById("reg2").innerHTML=
+    document.getElementById("p").innerHTML=
     "I look forward to speaking with you soon";
     }
-</script>
 
-<button onclick="myFunction('click')">Sign Up!</button>
+
