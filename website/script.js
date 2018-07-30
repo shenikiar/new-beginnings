da5f23be6f3c3886b261dfdda012f7a1486d66d7
shenikiar <39305056+shenikiar@users.noreply.github.com>
2018-07-30 00:17:37 -0400

    Update script.js

--- website/script.js
+++ website/script.js
@@ -6,38 +6,6 @@ $('.spoiler button').click(function(){
   //Show the Welcome text
 $('.spoiler span').show();
   //Hide the "Welcome" Button
-  $('.spoiler button').fadeOut();
+  $('.spoiler button').hide();
 });
 
-
-<<<<<<< HEAD
-<script>
-//When the user clicks on <div>, open the popuptext
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-
-popup.classList.toggle("show");
-}
-</script>
-
-// Hide Thanks text
-$('.popup span').hide();
-//When the button is pressed
-$('.popup button').click(function(){
-  //Show the Thanks text
-$('.popup span').show();
-  //Hide the "Thanks" Button
-$('.popup button').hide();
-
-});
-=======
-
-function myFunction() {
-    document.getElementById("p").innerHTML=
-    "Thank you for signing up!";
-    document.getElementById("p").innerHTML=
-    "I look forward to speaking with you soon";
-    }
-
-
->>>>>>> 7b1bc0352e73d280219dc416475b5f8170ad7b90
